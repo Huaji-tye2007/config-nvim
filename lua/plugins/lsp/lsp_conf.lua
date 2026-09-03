e2dcbf828b1b756f903cf3000b2866d4e9531047
Yu'en Tang <251220100@smail.nju.edu.cn>
2026-09-03 14:16:15 +0800

    add inlay hint support

--- lua/plugins/lsp/lsp_conf.lua
+++ lua/plugins/lsp/lsp_conf.lua
@@ -50,3 +50,13 @@ vim.lsp.enable({
     "vide", -- verilog
     "clangd",
 })
+
+-- inlay hints
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client:supports_method("textDocument/inlayHint") then
+            vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+        end
+    end,
+})
--- lua/plugins/lsp/lua.lua
+++ lua/plugins/lsp/lua.lua
@@ -49,6 +49,15 @@ vim.lsp.config('lua_ls', {
                 completion = {
                     callSnippet = "Replace",
                 },
+                hint = {
+                    enable = true,       -- Enable inlay hints globally
+                    paramName = "All",   -- Parameter name hints: "All" | "Literal" | "Disable"
+                    paramType = true,    -- Function parameter type hints
+                    setType = true,      -- Assignment type hints
+                    arrayIndex = "Auto", -- Array index hints: "Enable" | "Auto" | "Disable"
+                    await = true,        -- Await hints
+                    semicolon = "All",   -- Semicolon hints: "All" | "SameLine" | "Disable"
+                },
             }
         })
     end,
